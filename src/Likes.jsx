690c3354081d15e6e5d37037e4a994565b32ca1f
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-15 19:17:10 +0200

    add Likes component

---
+++ src/Likes.jsx
@@ -0,0 +1,11 @@
+function Likes(props) {
+    console.log(props);
+    return (
+        <div className='button-controls'>
+            <button>🖤 {props.likes}</button>
+            <button>Dislike</button>
+        </div>
+    );
+}
+
+export default Likes;
