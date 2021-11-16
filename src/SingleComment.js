9605eaee7c8b8e003d957d058ac56383aea49cfd
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-16 20:27:00 +0200

    display comments

--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,12 +1,26 @@
-const SingleComment = ({ data }) => {
+import { useEffect, useState } from 'react';
+
+function SingleComment({ data }) {
+    const [commentText, setCommentText] = useState('');
     const { text, id } = data;
+
+    useEffect(() => {
+        if (text) {
+            setCommentText(text);
+        }
+    }, [text]);
+
+    const handleInput = (e) => {
+        setCommentText(e.target.value);
+    };
+
     return (
         <form className='comments-item'>
             <div className='comments-item-delete'>&times;</div>
-            <input type='text' />
+            <input type='text' value={commentText} onChange={handleInput} />
             <input type='submit' hidden />
         </form>
     );
-};
+}
 
 export default SingleComment;
