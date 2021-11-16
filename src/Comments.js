90ac3fa4ea2dfa96c172dd5a51bd2b3e1572eb54
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-16 19:31:32 +0200

    add comment component

---
+++ src/Comments.js
@@ -0,0 +1,15 @@
+function Comments(props) {
+    console.log('comments props >>', props);
+
+    return (
+        <div className='card-comments'>
+            <form className='comments-item-create'>
+                <input type='text' />
+                <input type='submit' hidden />
+            </form>
+            <SingleComment />
+        </div>
+    );
+}
+
+export default Comments;
