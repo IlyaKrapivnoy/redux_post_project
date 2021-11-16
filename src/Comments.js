cee8d04cfe0f33aac1ccb798d2c6446747b72afa
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-16 19:42:02 +0200

    get value from input

--- src/Comments.js
+++ src/Comments.js
@@ -1,12 +1,19 @@
 import SingleComment from './SingleComment';
+import { useState } from 'react';
 
 function Comments(props) {
-    console.log('comments props >>', props);
+    // console.log('comments props >>', props);
+    const [textComment, setTextComment] = useState('');
+
+    const handleInput = (e) => {
+        console.log('input >>', e.target.value);
+        setTextComment(e.target.value);
+    };
 
     return (
         <div className='card-comments'>
             <form className='comments-item-create'>
-                <input type='text' />
+                <input type='text' value={textComment} onChange={handleInput} />
                 <input type='submit' hidden />
             </form>
             <SingleComment />
