f1892b09fc9b6888d5274f5587b91f912e1f92ee
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-17 19:41:40 +0200

    add error types

--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -1,7 +1,13 @@
-import { LOADER_DISPLAY_ON, LOADER_DISPLAY_OFF } from './types';
+import {
+    LOADER_DISPLAY_ON,
+    LOADER_DISPLAY_OFF,
+    ERROR_DISPLAY_ON,
+    ERROR_DISPLAY_OFF,
+} from './types';
 
 const initialState = {
     loading: false,
+    error: null,
 };
 
 export const appReducer = (state = initialState, action) => {
@@ -13,10 +19,23 @@ export const appReducer = (state = initialState, action) => {
                 ...state,
                 loading: true,
             };
+
         case LOADER_DISPLAY_OFF:
             return {
                 ...state,
                 loading: false,
+            };
+
+        case ERROR_DISPLAY_ON:
+            return {
+                ...state,
+                error: action.text,
+            };
+
+        case ERROR_DISPLAY_OFF:
+            return {
+                ...state,
+                error: null,
             };
 
         default:
