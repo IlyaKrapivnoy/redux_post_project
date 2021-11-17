37ceab2a944afe1f749caf65232090e298092481
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-17 20:09:34 +0200

    if hasBadWords

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,10 +1,19 @@
 import { COMMENT_CREATE } from './types';
+import { errorOn } from './actions';
+
+const badWords = ['suck', 'shit', 'asshole', 'fuck'];
 
 export function spamFilter(store) {
     return function (next) {
         return function (action) {
             if (action.type === COMMENT_CREATE) {
-                console.log('spanFilter >>', action.data.text);
+                console.log('spamFilter >>', action.data.text);
+                const hasBadWords = badWords.some((res) =>
+                    action.data.text.includes(res)
+                );
+                if (hasBadWords) {
+                    return store.dispatch(errorOn('DO NOT USE BAD WORDS PLS!'));
+                }
             }
             return next(action);
         };
